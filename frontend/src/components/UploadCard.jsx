4bfce7f329e1bc0e1a0822109781054225730a6f
Ahsan <ahsan.bagsaria@gmail.com>
2026-07-30 12:50:04 +0530

    Updated API URL

--- frontend/src/components/UploadCard.jsx
+++ frontend/src/components/UploadCard.jsx
@@ -47,7 +47,7 @@ function UploadCard({ darkMode, setPrediction, setLoading, setError }) {
         try {
             const formData = new FormData();
             formData.append("file", image);
-
+            console.log("API URL:", API_URL);
             const response = await fetch(`${API_URL}/predict`, {
                 method: "POST",
                 body: formData,

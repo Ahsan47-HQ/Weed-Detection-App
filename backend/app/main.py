505d21a6b04bc4b8af49f72232ef4148bee2467a
Ahsan <ahsan.bagsaria@gmail.com>
2026-07-30 12:01:13 +0530

    Updated CORS config

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:5173",
-    "https://weed-detection-app.vercel.app/",
+    "https://weed-detection-app.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],

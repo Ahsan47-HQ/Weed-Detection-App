5b79e00023d7cd5f45c1d894ab3c207172c3b580
Ahsan <ahsan.bagsaria@gmail.com>
2026-08-16 12:02:20 +0530

    Dockerized application

--- frontend/src/components/UploadCard.jsx
+++ frontend/src/components/UploadCard.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "/api"; // for Docker + nginx
 
 function UploadCard({ darkMode, setPrediction, setLoading, setError }) {
     const [image, setImage] = useState(null);
